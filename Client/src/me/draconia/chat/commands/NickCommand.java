b57b025f4fa44a01796b2121e13c490054ff1c58
Doridian Draconia <mriq91@gmail.com>
2013-02-01 00:23:22 +0100

    Added /nick

---
+++ Client/src/me/draconia/chat/commands/NickCommand.java
@@ -0,0 +1,16 @@
+package me.draconia.chat.commands;
+
+import me.draconia.chat.client.ClientLib;
+import me.draconia.chat.net.packets.PacketNickset;
+import me.draconia.chat.types.MessageContext;
+import me.draconia.chat.types.User;
+
+@BaseCommand.Names("nick")
+public class NickCommand extends BaseClientCommand {
+    @Override
+    public void run(User user, MessageContext messageContext, String[] args, String argStr) throws Exception {
+        PacketNickset packetNickset = new PacketNickset();
+        packetNickset.nickname = args[0];
+        ClientLib.sendPacket(packetNickset);
+    }
+}
